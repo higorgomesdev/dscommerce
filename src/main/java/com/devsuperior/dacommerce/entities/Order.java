aeebccec3c6ff66065d56751a96918daeaf7f5fe
Higor Gomes <higorddtank123@gmail.com>
2026-04-14 11:48:54 -0300

    Payment

--- src/main/java/com/devsuperior/dacommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dacommerce/entities/Order.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 
 import com.devsuperior.dacommerce.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -29,14 +31,18 @@ public class Order {
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+	
 	public Order() {
 	}
 	
-	public Order(Long id, Instant moment, OrderStatus status, User client) {
+	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
 		this.id = id;
 		this.moment = moment;
 		this.status = status;
 		this.client = client;
+		this.payment = payment;
 	}
 
 	public Long getId() {
@@ -69,6 +75,14 @@ public class Order {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	@Override

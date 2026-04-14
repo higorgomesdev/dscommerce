a5efc42b0eaafbe3f3224c73d593e00794c38ba2
Higor Gomes <higorddtank123@gmail.com>
2026-04-14 10:56:00 -0300

    entity User

---
+++ src/main/java/com/devsuperior/dacommerce/DacommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dacommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DacommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DacommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dacommerce/DacommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dacommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DacommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
